ebbad0b4aa65fe6cb6cee4f8c948e4682c6a04fa
Kovács-Ferenc Norbert <kfnorbi@gmail.com>
2016-12-15 21:22:37 +0100

    Build fix

--- nvk-web/src/main/java/hu/unideb/inf/nvkshop/rest/PasswordRecoveryRest.java
+++ nvk-web/src/main/java/hu/unideb/inf/nvkshop/rest/PasswordRecoveryRest.java
@@ -2,23 +2,29 @@ package hu.unideb.inf.nvkshop.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import hu.unideb.inf.rft.nvkshop.service.DeletedEntityException;
 import hu.unideb.inf.rft.nvkshop.service.UserPasswordRecoveryService;
 
 @RestController
-public class PasswordRecovery {
+public class PasswordRecoveryRest {
 
 	@Autowired
 	private UserPasswordRecoveryService userPasswordRecoveryService;
 
 	@RequestMapping(value = "/forgottenPassword", method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.OK)
-	public void createUserPasswordRecovery(@RequestParam("email") String email) {
-		userPasswordRecoveryService.createUserPasswordRecoveryByEmail(email);
+	public ResponseEntity<?> createUserPasswordRecovery(@RequestParam("email") String email) {
+		try {
+			userPasswordRecoveryService.createUserPasswordRecoveryByEmail(email);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (DeletedEntityException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
 }
